19973dd32f569da953d86e90b0a3d4ba86c2a9c3
Mathias Dumain <mathias.dumain@edu.univ-fcomte.fr>
2024-10-17 10:20:09 +0200

    update En ce moment
    popup actif
    Style remis

--- js_moment.js
+++ js_moment.js
@@ -3,8 +3,26 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('bouton1').addEventListener('click', function() {
         document.getElementById('dialog1').showModal();
     });
+    document.getElementById('bouton2').addEventListener('click', function() {
+        document.getElementById('dialog2').showModal();
+    });
+    document.getElementById('bouton3').addEventListener('click', function() {
+        document.getElementById('dialog3').showModal();
+    });
+    document.getElementById('bouton4').addEventListener('click', function() {
+        document.getElementById('dialog4').showModal();
+    });
 
-    document.getElementById('closeDialog').addEventListener('click', function() {
+    document.getElementById('closeDialog1').addEventListener('click', function() {
         document.getElementById('dialog1').close();
+    });
+    document.getElementById('closeDialog2').addEventListener('click', function() {
+        document.getElementById('dialog2').close();
+    });
+    document.getElementById('closeDialog3').addEventListener('click', function() {
+        document.getElementById('dialog3').close();
+    });
+    document.getElementById('closeDialog4').addEventListener('click', function() {
+        document.getElementById('dialog4').close();
     });
 });
